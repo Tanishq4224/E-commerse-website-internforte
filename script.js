566438965d98dddd89a5c47f927d64f57a117acb
Tanishq4224 <tanishq4224@gmail.com>
2024-07-01 15:14:50 +0530

    Add files via upload
    added home and 4 other pages 
    added filter code to electronics page

---
+++ script.js
@@ -0,0 +1,21 @@
+const filterButtons = document.querySelectorAll('.filter-buttons button')
+const fiterableCards = document.querySelectorAll('.fiterable-cards .card')
+
+
+
+const filterCards = e => {
+    document.querySelector(".active")?.classList.remove("active")
+    e.target.classList.add("active")
+    fiterableCards.forEach(card => {
+        card.classList.add('hide')
+        if (card.dataset.name === e.target.dataset.name || e.target.dataset.name == 'all') {
+            card.classList.remove('hide')
+        }
+    })
+
+
+}
+
+
+
+filterButtons.forEach(button => button.addEventListener("click", filterCards))
